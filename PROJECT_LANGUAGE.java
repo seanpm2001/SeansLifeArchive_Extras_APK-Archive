000e76a6bcd968aec59ae1b929b6f9b3b24787a3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-01 19:52:05 -0800

    PROJECT_LANGUAGE.java
    The project language file for this project.

---
+++ PROJECT_LANGUAGE.java
@@ -0,0 +1,13 @@
+// Start of script
+public class projectLanguage {
+    public static void main(String[] args) {
+        System.out.println("Android is written in Java, so I thought that would fit as a project language for this project. Unfortunately, this is currently being overshadowed by all the HTML and CSS data in the Jewels extraction set."); // Prints the string to the console.
+    }
+}
+// Android is written in Java, so I thought that would fit as a project language for this project. Unfortunately, this is currently being overshadowed by all the HTML and CSS data in the Jewels extraction set.
+/* File info
+* File version: 1 (Sunday, November 1st 2020 at 7:52 am)
+* File type: Java script file (*.java)
+* Line count (including blank lines and compiler line): 14
+*/
+// End of script
